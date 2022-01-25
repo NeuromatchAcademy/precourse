32084c5973894519b08ff2b128fd56818cb3fdbb
Spiros Ch <spirosChv@users.noreply.github.com>
2022-01-25 17:47:20 +0200

    Update publish-book.yaml

--- tutorials/W0D4_Calculus/solutions/W0D4_Tutorial1_Solution_6967496a.py
+++
@@ -1,8 +0,0 @@
-
-"""
-1) Change terms involving x to just be linear (a constant times x, or a constant times x times some function of y).
-Change a term involving y to involve more than y or y^2. For example, x +  2*x*y + y**3
-
-2) If there are no terms involving both x and y, the partial derivative with respect to x will
-   just depend on x and the partial derivative with respect to y will just depend on y.
-"""
