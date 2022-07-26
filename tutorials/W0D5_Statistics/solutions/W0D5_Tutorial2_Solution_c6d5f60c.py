802a15622f350396abcb00d9f15dcce89ab31bc1
Spiros Ch <spirosChv@users.noreply.github.com>
2022-07-26 14:23:11 +0300

    Minor fixes (#59)
    * typos style
    
    * fixes style
    
    * minor fix
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W0D5_Statistics/solutions/W0D5_Tutorial2_Solution_c6d5f60c.py
+++ tutorials/W0D5_Statistics/solutions/W0D5_Tutorial2_Solution_c6d5f60c.py
@@ -6,8 +6,7 @@ Pw1r0s0 = 0.001  # the probability of wet grass given no rain and sprinklers off
 Ps = 0.25  # the probability of the sprinkler being on
 Pr = 0.1   # the probability of rain that day
 
-# Uncomment once variables are assigned above
-A= Ps * (Pw1r1s1 * Pr + (Pw1r0s1) * (1 - Pr))
-B= (1 - Ps) * (Pw1r1s0 *Pr + (Pw1r0s0) * (1 - Pr))
-print("Given that the grass is wet, the probability the sprinkler was on is: " +
-      str(A/(A + B)))
+# Calculate A and B
+A = Ps * (Pw1r1s1 * Pr + (Pw1r0s1) * (1 - Pr))
+B = (1 - Ps) * (Pw1r1s0 *Pr + (Pw1r0s0) * (1 - Pr))
+print(f"Given that the grass is wet, the probability the sprinkler was on is: {A/(A + B):.4f}")
